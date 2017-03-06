313e8baca3d2e24efec6e99c6823e898af1720c2
Maxim Mushko <mmushko@griddynamics.com>
2017-03-06 14:42:49 +0400

    Delete hello message

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String [] args){
         System.out.print("Hello World");
         System.out.print("Hello from github");
-        System.out.print("Hello from webhook");
     }
 }
