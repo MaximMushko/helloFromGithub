e078e85301e4e4dc090ef6654620350ebd3eefb5
Maxim Mushko <mmushko@griddynamics.com>
2017-03-23 15:26:28 +0400

    add unit-test

---
+++ test/SimpleTests.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by mmushko on 23/03/17.
+ */
+public class SimpleTests {
+
+    @Test
+    public void myTest(){
+        assertEquals(true,false);
+    }
+}
